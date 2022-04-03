940dbd6f8cc562f9864c73b44483b53fb3e3321a
bree parker <breannaparker1991@gmail.com>
2022-04-03 00:08:36 -0600

    basic form created

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-from env import KEY
 from flask import Flask, render_template, session, request, redirect
 from env import KEY
 
@@ -6,10 +5,22 @@ app = Flask(__name__)
 app.secret_key = KEY
 
 @app.route('/')
-def main():
+def first():
   return render_template("index.html")
 
-# @app.route('/result' method=["POST"])
+@app.route('/main', methods=["POST"])
+def main():
+  session['name'] = request.form['name']
+  session['location'] = request.form['location']
+  session['langauge'] = request.form['language']
+  session['comment'] = request.form['comment']
+  return redirect ('/results')
+
+@app.route('/results')
+def results():
+  return render_template("submit.html", name=session['name'], 
+  location=session['location'], language=session['langauge'],
+  comment=session['comment'])
 
 if __name__ == "__main__":
   app.run(debug=True)
